9eeda6592ff642d81b11663a3e893ef4cf4d140d
rednax825 <rednax825@gmail.com>
2017-02-07 18:49:14 -0800

    made constructors for PID class

--- SteamWorksSwerve/src/org/usfirst/frc/team2733/robot/PID.java
+++ SteamWorksSwerve/src/org/usfirst/frc/team2733/robot/PID.java
@@ -2,15 +2,15 @@ package org.usfirst.frc.team2733.robot;
 
 public class PID {
 
-	public PID(double Pcoef){
+	public PID(double P_coef){
 		
 	}
 	
-	public PID(double Pcoef){
+	public PID(double P_coef, double I_coef){
 		
 	}
 	
-	public PID(double Pcoef){
+	public PID(double P_coef, double I_coef, double D_coef){
 		
 	}
 }
