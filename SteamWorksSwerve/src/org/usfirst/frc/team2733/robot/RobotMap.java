32923e50c8dfc6d9f3516dd9e500e316844c937a
Benjamin Green <benjaminisaacgreen@gmail.com>
2017-02-09 09:49:41 -0800

    Create RobotMap.java
    Added a class to hold ALL of our variables and ports so that changing things will be much more intuitive and simple

---
+++ SteamWorksSwerve/src/org/usfirst/frc/team2733/robot/RobotMap.java
@@ -0,0 +1,22 @@
+package
+
+public class RobotMap {
+
+
+public static double P = 0.01;
+public static double I, D;
+
+public
+
+  public RobotMap() {
+  
+  }
+  
+  public void InitSwerveComp() {
+  
+  }
+  public void InitSwerveDev() {
+  
+  }
+
+}
