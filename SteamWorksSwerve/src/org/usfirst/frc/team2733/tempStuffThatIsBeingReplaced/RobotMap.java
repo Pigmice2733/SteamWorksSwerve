9d03d08986d4841c1741e2916d96eaa6fd4bd2e7
name <a;lsdkjf>
2017-02-09 17:20:22 -0800

    More RobotMap

--- SteamWorksSwerve/src/org/usfirst/frc/team2733/tempStuffThatIsBeingReplaced/RobotMap.java
+++ SteamWorksSwerve/src/org/usfirst/frc/team2733/tempStuffThatIsBeingReplaced/RobotMap.java
@@ -1,21 +1,21 @@
-package org.usfirst.frc.team2733.tempStuffThatIsBeingReplaced;
+package org.usfirst.frc.team2733.robot;
 
-import org.usfirst.frc.team2733.tempStuffThatIsBeingReplaced.SwerveModule.WheelName;
-import org.usfirst.frc.team2733.robot.PID;
-import org.usfirst.frc.team2733.robot.driveTrain.AbsoluteEncoder;
-
-import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Talon;
+
+import org.usfirst.frc.team2733.robot.SwerveModule.WheelName;
+
+import com.ctre.CANTalon;
 
 public class RobotMap {
 
 	/** P value to be used in internal PID loop. */
-	public static double P = 0.01;
+	public static double PDrive = 0.01;
+	public static double PRotate = 0.01;
 	/** I value to be used in internal PID loop. */
-	public static double I = 0;
+	public static double IDrive = 0;
 	/** D value to be used in internal PID loop. */
-	public static double D = 0;
+	public static double DDrive = 0;
 
 	public static SwerveModule FR;
 	/** The front right swerve module. */
@@ -25,42 +25,48 @@ public class RobotMap {
 	/** The back right swerve module. */
 	public static SwerveModule BR;
 	
-	public static PID pid;
-	
-	//public static double minSetpoint = 1.5;
+	public static double minSetpoint = 1.5;
 	/** The highest setpoint the steering motor should be allowed to go to. */
-	//public static double maxSetpoint = 3.5;
+	public static double maxSetpoint = 3.5;
 
 	public static int joystickPort1 = 0;
 	public static int joystickPort2 = 0;
 
+	public enum WheelCoords {
+		FR(0.5, 0.5),
+		FL(0.5, 0.5),
+		BR(0.5, 0.5),
+		BL(0.5, 0.5);
+		
+		private double x, y;
+		private WheelCoords(double x, double y) {
+			this.x = x;
+			this.y = y;
+		}
+		public double getX() { return x;}
+		public double getY() { return y;}
+	}
 
-	/** Initialize Talons and encoders for each swerve module. */
+	/** Initialize Talons and encoders for each swerve module for competition. */
 	public static void initSwerve1() {// Swerve Initialization for Competition Swerve
 		FR = new SwerveModule(WheelName.FR, new AbsoluteEncoder(4, 1, 0), new CANTalon(11), new Spark(0));// FORMAT: Name, Turning Encoder, Drive Motor, turning Motor
 		FL = new SwerveModule(WheelName.FL, new AbsoluteEncoder(5, 1, 0), new CANTalon(12), new Spark(1));// Same Format for other inits
 		BR = new SwerveModule(WheelName.BR, new AbsoluteEncoder(6, 1, 0), new CANTalon(13), new Spark(2));
 		BL = new SwerveModule(WheelName.BL, new AbsoluteEncoder(7, 1, 0), new CANTalon(14), new Spark(3));
 	}
-
+	/**Initialize Talons and encoders for each swerve module for development.*/
 	public static void initSwerve2() {// Swerve Initialization for Development Swerve. The only difference is the speed controller types used.
 		FR = new SwerveModule(WheelName.FR, new AbsoluteEncoder(4, 1, 0), new CANTalon(11), new Talon(0));// FORMAT: Name, Turning Encoder, Drive Motor, turning Motor
 		FL = new SwerveModule(WheelName.FL, new AbsoluteEncoder(5, 1, 0), new CANTalon(12), new Talon(1));// Same Format for other inits
 		BR = new SwerveModule(WheelName.BR, new AbsoluteEncoder(6, 1, 0), new CANTalon(13), new Talon(2));
 		BL = new SwerveModule(WheelName.BL, new AbsoluteEncoder(7, 1, 0), new CANTalon(14), new Talon(3));
 	}
-
+	
+	/**Initialize Talons and encoders for swerve modules in tank drive mode.*/
 	public static void initTank() {// In case we use tank drive, this is a backup. Hopefully we never use this.
 		
 	}
 	
-	public void initPID() {//Initializes the PID class. Needs to be called in the RobotInit
-		pid = new PID(P);//Change the Init to add I and D IF necessary.
-	}
-	
-	public void initOtherMovingParts() {//Initializes the other components of the robot such as the Shooter, Ball intake, etc. 
-		//UNFINISHED//
-	}
 	
 	public static double mapRange(double value, double fromLow, double fromHigh, double toLow, double toHigh) {
         return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
