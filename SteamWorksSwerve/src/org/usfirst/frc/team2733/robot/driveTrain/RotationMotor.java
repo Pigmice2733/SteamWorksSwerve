bbc5edd8c1ab55ed724f1915c25f452fa9da7ede
rednax825 <rednax825@gmail.com>
2017-02-14 18:33:21 -0800

    Improved variable names

--- SteamWorksSwerve/src/org/usfirst/frc/team2733/robot/driveTrain/RotationMotor.java
+++ SteamWorksSwerve/src/org/usfirst/frc/team2733/robot/driveTrain/RotationMotor.java
@@ -8,29 +8,29 @@ import edu.wpi.first.wpilibj.Spark;
 
 public class RotationMotor {
 
-	private final Spark rotMotor;
+	private final Spark rotationMotor;
 	private final Encoder encoder;
-	private final PID piController;
+	private final PID PIController;
 	
 	public RotationMotor(int motorPort, int encoderPortA, int encoderPortB, double P, double I){
-	    rotMotor = new Spark(motorPort);
+	    rotationMotor = new Spark(motorPort);
 		encoder = new Encoder(encoderPortA, encoderPortB);
 		
 		// Encoder will output distance as radians
 		encoder.setDistancePerPulse(ConversionEnum.RELATIVE_ENCODER_PULSES_TO_RADIANS.getConversion());
 		
-		piController = new PID(P, I); 
-		piController.setMaxMin(2 * Math.PI, 0);
-		piController.setContinuous(true);
+		PIController = new PID(P, I); 
+		PIController.setMaxMin(2 * Math.PI, 0);
+		PIController.setContinuous(true);
 	}
 	
 	// Get angle to drive at, update PID and motor
 	public void update(double angle) {
-		double piVal = piController.getVal(encoder.getDistance() % (2 * Math.PI), angle);
-		rotMotor.set(piVal);
+		double piVal = PIController.getVal(encoder.getDistance() % (2 * Math.PI), angle);
+		rotationMotor.set(piVal);
 	}
 	
 	public void disable() {
-		rotMotor.disable();
+		rotationMotor.disable();
 	}
 }
